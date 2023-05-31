f2de7fcf0adfe64c0f68c6c67bca498352a77bee
Nahid Hasan <nahidjc80@gmail.com>
2023-05-31 12:52:16 +0600

    user model created

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { DB_URL, DB_TEST_URL, NODE_ENV } from "./variables";
-const uri: string = NODE_ENV === "test" ? DB_TEST_URL! : DB_URL!;
+const uri: string = NODE_ENV === "test" ? DB_TEST_URL : DB_URL;
 mongoose.set("strictQuery", false);
 mongoose.connect(uri);
 const db = mongoose.connection;
