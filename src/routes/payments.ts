d583bb66e89855050cf5159c8e4992156991e9f2
Nahid Hasan <nahidjc80@gmail.com>
2023-06-15 13:23:37 +0600

    fetched transactions history

--- src/routes/payments.ts
+++ src/routes/payments.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createCashoutPayment, expensesController } from "../controllers/payment";
+import { createCashoutPayment, expensesController, getUserTransactions } from "../controllers/payment";
 
 const router: Router = Router();
 
 router.post("/payment/cashout", createCashoutPayment);
 router.get("/today/expense", expensesController);
+router.get("/transactions", getUserTransactions);
 
 export default router;
